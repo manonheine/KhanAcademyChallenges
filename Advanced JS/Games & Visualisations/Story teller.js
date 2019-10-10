328be657dfd0d1277c251deed515088e107cd433
Manon Heine <30656550+manonheine@users.noreply.github.com>
2019-10-10 11:40:39 +0200

    Scene management > Challenge: Story teller

--- Advanced JS/Games & Visualisations/Story teller.js
+++ Advanced JS/Games & Visualisations/Story teller.js
@@ -1,29 +1,29 @@
-## Step 1. 
-You're going to tell the story of Leafers, who starts as a seed and grows to Leafer's ultimate form, using scenes.
+// ## Step 1. 
+// You're going to tell the story of Leafers, who starts as a seed and grows to Leafer's ultimate form, using scenes.
 
-We've provided you with, drawScene1, a scene drawing function for the first scene where Leafers is just a seed, and drawScene5, a scene drawing function for the fifth (and last) scene where Leafers is in ultimate form.
+// We've provided you with, drawScene1, a scene drawing function for the first scene where Leafers is just a seed, and drawScene5, a scene drawing function for the fifth (and last) scene where Leafers is in ultimate form.
 
-Your task for this step is to add logic to the mouseClicked function so that :
-- If the first scene is displayed and you click the mouse, it will display the last scene
-- If the last scene is displayed and you click the mouse, it will display the first scene
+// Your task for this step is to add logic to the mouseClicked function so that :
+// - If the first scene is displayed and you click the mouse, it will display the last scene
+// - If the last scene is displayed and you click the mouse, it will display the first scene
 
-## Step 2. 
-Add the second, third and fourth scenes to fill in the rest of the story.
+// ## Step 2. 
+// Add the second, third and fourth scenes to fill in the rest of the story.
 
-Each scene should:
-- update the scene index, currentScene, to the scene number
-- draw a unique background
-- display the image of Leafers
-- have text describing the scene
+// Each scene should:
+// - update the scene index, currentScene, to the scene number
+// - draw a unique background
+// - display the image of Leafers
+// - have text describing the scene
 
-In the second scene Leafers should be a seedling, in the third scene Leafers should be a sapling, and in the fourth scene Leafers should be a tree.
+// In the second scene Leafers should be a seedling, in the third scene Leafers should be a sapling, and in the fourth scene Leafers should be a tree.
 
-Don't worry about adding logic to mouseClicked to display these scenes in this step. You will do that next step.
+// Don't worry about adding logic to mouseClicked to display these scenes in this step. You will do that next step.
 
-## Step 3. 
-Modify the program so that clicking on the mouse shows the scenes in order.
+// ## Step 3. 
+// Modify the program so that clicking on the mouse shows the scenes in order.
 
-Clicking the mouse on the last scene should still make the first scene appear.
+// Clicking the mouse on the last scene should still make the first scene appear.
 
 fill(0, 0, 0);
 textSize(20);
